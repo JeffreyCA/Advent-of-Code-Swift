f764cdb5c5929589370e562d6647c130ec75bea9
JeffreyCA <jeffreyca16@gmail.com>
2018-12-01 10:26:53 -0500

    Complete Day 1 Part 2

--- Day 1/main.swift
+++ Day 1/main.swift
@@ -22,16 +22,55 @@ func main() {
         return
     }
     
+    // freqChangeay storing frequency changes
+    var freqChange = [Int]()
+    // Set storing encountered frequencies
+    var set = Set<Int>()
+    // Current frequency
     var frequency = 0
+    var foundDupe = false
     
     for line in reader {
         autoreleasepool {
-            let line = line.trimmingCharacters(in: .whitespacesAndNewlines)
-            frequency += freqChangeToInt(line: line)
+            let trimmed = line.trimmingCharacters(in: .whitespacesAndNewlines)
+            let change = freqChangeToInt(line: trimmed)
+            
+            frequency += change
+            freqChange.append(change)
+            
+            if set.contains(frequency) {
+                print("Duplicate frequency: " + String(frequency))
+                foundDupe = true
+            }
+            set.insert(frequency)
         }
     }
     
-    print(frequency)
+    print("Final frequency: " + String(frequency))
+    
+    if foundDupe {
+        return
+    }
+    
+    var index = 0
+    
+    while true {
+        // Loop back to beginning of frequency change list
+        if index == freqChange.count {
+            index = 0
+        }
+        
+        frequency += freqChange[index]
+        
+        if set.contains(frequency) {
+            print("Duplicate frequency: " + String(frequency))
+            break
+        }
+        
+        // Store encountered frequencies in set
+        set.insert(frequency)
+        index += 1
+    }
 }
 
 main()
