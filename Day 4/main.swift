14ee24fed8bcd13bbef53fb7aaf6efade1b8aa69
JeffreyCA <jeffreyca16@gmail.com>
2018-12-04 11:12:10 -0500

    Tidy up code

--- Day 4/main.swift
+++ Day 4/main.swift
@@ -5,67 +5,79 @@
 
 import Foundation
 
-func matches(for regex: String, in text: String) -> [String] {
-    
-    do {
-        let regex = try NSRegularExpression(pattern: regex)
-        let results = regex.matches(in: text,
-                                    range: NSRange(text.startIndex..., in: text))
-        return results.map {
-            String(text[Range($0.range, in: text)!])
-        }
-    } catch let error {
-        print("Invalid regex: \(error.localizedDescription)")
-        return []
-    }
-}
-
+// Parse line to get the minute (0 - 59)
 func getMinute(_ line: String) -> Int {
-    let match = Int(matches(for: "(?<=:)(\\d+)", in: line)[0])!
-    return match
+    let minute = Int(matches(for: "(?<=:)(\\d+)", in: line)[0])!
+    return minute
 }
 
+// Parse line to get the guard ID
 func getGuard(_ line: String) -> Int {
-    let match = Int(matches(for: "(?<=#)(\\d+)", in: line)[0])!
-    let guardId = Int(line.components(separatedBy: " ")[3][1...])!
-    assert(match == guardId)
-    return match
+    let guardId = Int(matches(for: "(?<=#)(\\d+)", in: line)[0])!
+    return guardId
 }
 
 func main() {
     let lines = readInput()
     let sorted = lines.sorted()
+    
+    // Dict of guard data, maps guard ID to tuple of sleep time and frequency array
+    // of number of times asleep at each minute (0 - 59)
     var dict = Dictionary<Int, (Int, [Int])>()
     
-    var guardId = 0
+    // Current guard ID being processed
+    var curGuardId = 0
+    // Minute at which current guard begins to sleep
     var beginSleep = 0
+    // Max number of times a guard is asleep during a minute
+    var mostFreqCount = 0
+    // Specific minute during which a guard is asleep the most number of times
+    var mostFreqMinute = 0
+    // Guard ID of guard who is asleep the most number of times during a minute
+    var mostFreqGuardId = 0
     
     for line in sorted {
         let minute = getMinute(line)
         
         if line.contains("begins shift") {
-            guardId = getGuard(line)
-            
-            if dict[guardId] == nil {
-                dict[guardId] = (0, [Int](repeating: 0, count: 60))
+            // Process new or existing guard
+            curGuardId = getGuard(line)
+            // Add entry to dict if non-existant
+            if dict[curGuardId] == nil {
+                dict[curGuardId] = (0, [Int](repeating: 0, count: 60))
             }
         } else if line.contains("falls asleep") {
+            // Update time at which current guard begins sleeping
             beginSleep = minute
         } else if line.contains("wakes up") {
-            dict[guardId]?.0 += minute - beginSleep + 1
+            // Calculate time asleep for current guard
+            dict[curGuardId]?.0 += minute - beginSleep + 1
+            
             for i in beginSleep ... minute {
-                dict[guardId]?.1[i] += 1
+                // Update frequency array for current guard
+                dict[curGuardId]?.1[i] += 1
+                
+                if (dict[curGuardId]?.1[i])! > mostFreqCount {
+                    // Track the most frequent minute spent asleep by a guard
+                    // across all guards (Part 2)
+                    mostFreqMinute = i
+                    mostFreqCount = (dict[curGuardId]?.1[i])!
+                    mostFreqGuardId = curGuardId
+                }
             }
         }
     }
     
+    // Part 1 calculations
     let maxKeyValue = dict.max { a, b in a.value.0 < b.value.0 }!
-    let maxGuardId = maxKeyValue.key
-    let maxMinute = maxKeyValue.value.1.enumerated().max(by: {$0.element < $1.element})!.offset
-    print("Guard: \(maxGuardId), minute: \(maxMinute)")
-    print("Answer: \(maxGuardId * maxMinute)")
+    let mostSleepGuardId = maxKeyValue.key
+    let mostSleepMinute = maxKeyValue.value.1.enumerated().max(by: {$0.element < $1.element})!.offset
     
-    
+    print("Guard with most sleep: \(mostSleepGuardId), Minute with most sleep: \(mostSleepMinute)")
+    print("Strategy 1: \(mostSleepGuardId * mostSleepMinute)")
+    // Part 2 calculation
+    print("Most frequent minute spent asleep: \(mostFreqMinute), Guard: \(mostFreqGuardId)")
+    print("Strategy 2: \(mostFreqMinute * mostFreqGuardId)")
 }
 
 main()
---
+++ Shared/Utils.swift
@@ -0,0 +1,31 @@
+//
+//  Advent of Code 2018
+//  Author: JeffreyCA
+//
+
+import Foundation
+
+func readInput() -> [String] {
+    do {
+        let contents = try String(contentsOfFile: "input.txt", encoding: String.Encoding.utf8)
+        let lines: [String] = contents.components(separatedBy: "\n").filter{$0 != ""}
+        return lines
+    } catch let error as NSError {
+        print(error.localizedDescription)
+        return [String]()
+    }
+}
+
+func matches(for regex: String, in text: String) -> [String] {
+    do {
+        let regex = try NSRegularExpression(pattern: regex)
+        let results = regex.matches(in: text,
+                                    range: NSRange(text.startIndex..., in: text))
+        return results.map {
+            String(text[Range($0.range, in: text)!])
+        }
+    } catch let error {
+        print("Invalid regex: \(error.localizedDescription)")
+        return []
+    }
+}
