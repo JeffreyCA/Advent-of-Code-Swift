03b218650ca1abd189911e9cd37d620dbc11a8e4
JeffreyCA <jeffreyca16@gmail.com>
2018-12-05 09:24:16 -0500

    Complete Day 5 Part 1

--- Day 5/main.swift
+++ Day 5/main.swift
@@ -5,9 +5,24 @@
 
 import Foundation
 
+func oppositeCase(_ char: Character) -> Character {
+    let LOWER_A = 97, UPPER_LOWER_OFFSET = 32
+    let val = Int(char.unicodeScalars.first!.value)
+    return Character(Unicode.Scalar(val >= LOWER_A ? val - UPPER_LOWER_OFFSET: val + UPPER_LOWER_OFFSET)!)
+}
+
 func main() {
-    let lines = readInput()
-    print(lines)
+    let line = readInput()[0]
+    var stack = [Character]()
+    
+    for c in line {
+        if !stack.isEmpty && stack.last == oppositeCase(c) {
+            stack.popLast()
+        } else {
+           stack.append(c)
+        }
+    }
+    print("Shortest polymer length: \(stack.count)")
 }
 
 main()
