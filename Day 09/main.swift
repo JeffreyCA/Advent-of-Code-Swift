522fabf63cc6dd039d2926eea9b33bcae8712e23
JeffreyCA <jeffreyca16@gmail.com>
2018-12-09 10:59:50 -0500

    Complete Day 9 Part 1

--- Day 09/main.swift
+++ Day 09/main.swift
@@ -6,8 +6,38 @@
 import Foundation
 
 func main() {
-    let line = readInput()
-    print(line)
+    let input = readInput().first!.components(separatedBy: " ")
+    let playerCount = Int(input[0])!
+    let lastMarble = Int(input[6])!
+    
+    // Double-ended queue representing the game
+    var deque = Deque<Int>()
+    deque.enqueueBack(0)
+    
+    // Player score counts
+    var scores = [Int].init(repeating: 0, count: playerCount)
+
+    for marble in 1 ... lastMarble - 1 {
+        let currentPlayer = marble % playerCount
+        
+        if marble % 23 == 0 {
+            // Add current marble to player score
+            scores[currentPlayer] += marble
+            // Remove the marble 7 positions counter-clockwise from the current marble and add to player score
+            deque.rotate(7)
+            scores[currentPlayer] += deque.dequeueBack()!
+            // Rotate queue backwards by one so that the marble clockwise to removed marble is the current marble
+            deque.rotate(-1)
+        } else {
+            // This series of operations ensures the current marble is always at the (right) end of the queue
+            // E.g. [1, 0, 2] becomes [0, 2, 1]
+            deque.rotate(-1)
+            // E.g. [0, 2, 1] becomes [0, 2, 1, 3], 3 is current marble
+            deque.enqueueBack(marble)
+        }
+    }
+    
+    print(scores.max()!)
 }
 
 main()
