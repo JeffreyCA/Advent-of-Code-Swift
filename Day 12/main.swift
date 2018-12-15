011386ab9390665d1294736c62e06974bfdb30c9
JeffreyCA <jeffreyca16@gmail.com>
2018-12-15 14:54:27 -0500

    Clean up code

--- Day 12/main.swift
+++ Day 12/main.swift
@@ -5,75 +5,70 @@
 
 import Foundation
 
-extension String {
-    subscript (bounds: CountableClosedRange<Int>) -> String {
-        let start = index(startIndex, offsetBy: bounds.lowerBound)
-        let end = index(startIndex, offsetBy: bounds.upperBound)
-        return String(self[start...end])
-    }
-    
-    subscript (bounds: CountableRange<Int>) -> String {
-        let start = index(startIndex, offsetBy: bounds.lowerBound)
-        let end = index(startIndex, offsetBy: bounds.upperBound)
-        return String(self[start..<end])
-    }
-}
-
-func main() {
-    let lines = readInput()
-    var initialState = matches(for: "(?<=initial state: ).*", in: lines[0])[0]
-    
-    var rules = [String : String]()
+// Parse input into initial state and dictionary of rules
+func parseInput(_ lines: [String]) -> (String, [String: String]) {
+    let initialState = matches(for: "(?<=initial state: ).*", in: lines[0])[0]
+    var rules = [String: String]()
     
     for index in 1 ..< lines.count {
         let state = matches(for: "[.#]+", in: lines[index])
         rules[state[0]] = state[1]
     }
     
+    return (initialState, rules)
+}
+
+// Run simulate with given rules and initial state for given amount of generations
+func simulate(with rules: [String: String], startingWith initialState: String, for generations: Int) -> Int {
     var zeroIndex = 0
     var previousSum = 0
     var diff = 0
+    var currentState = initialState
+    var sum = 0
     
-    for gen in 1 ... 50000000000 {
-        initialState = "....." + initialState + "....."
-        var newState = initialState
-        let end = initialState.count - 5
+    for gen in 1 ... generations {
+        currentState = "....." + currentState + "....."
         zeroIndex += 5
         
-        for mid in 2 ... end {
-            if let result = rules[initialState[mid - 2 ... mid + 2]] {
-                newState = newState[..<mid] + result + newState[(mid + 1)...]
+        let start = 2, end = currentState.count - 5
+        var nextState = currentState
+        
+        // Build next generation state by matching each substring of 5 with a rule
+        for mid in start ... end {
+            if let result = rules[currentState[mid - 2 ... mid + 2]] {
+                // Replace character at 'mid' with rule definition
+                nextState = nextState[..<mid] + result + nextState[(mid + 1)...]
             }
         }
         
-        initialState = newState
-        
-        var sum = 0
-        for (index, _) in initialState.enumerated() {
-            if initialState[index] == "#" {
+        // Calculate sum of indices the next state
+        sum = 0
+        for index in 0 ..< nextState.count {
+            if nextState[index] == "#" {
                 sum += (index - zeroIndex)
             }
         }
         
+        // Encounter consecutive sums with the same difference
+        // Manually calculate the overall sum
         if sum - previousSum == diff {
-            print("Break at \(gen)")
-            break
+            return (generations - gen) * diff + sum
         }
         
+        currentState = nextState
         diff = sum - previousSum
         previousSum = sum
-        print(sum)
     }
     
-    var sum = 0
+    return sum
+}
+
+func main() {
+    let lines = readInput()
+    let (initialState, rules) = parseInput(lines)
     
-    for (index, _) in initialState.enumerated() {
-        if initialState[index] == "#" {
-            sum += (index - zeroIndex)
-        }
-    }
-    
-    print(sum)
+    print("Sum after 20 generations: \(simulate(with: rules, startingWith: initialState, for: 20))")
+    print("Sum after 50000000000 generations: \(simulate(with: rules, startingWith: initialState, for: 50000000000))")
 }
 
 main()
--- Shared/Extension.swift
+++ Shared/Extension.swift
@@ -36,6 +36,20 @@ extension StringProtocol {
     }
 }
 
+extension String {
+    subscript (bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+    
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
+}
+
 extension Substring {
     var string: String { return String(self) }
 }
