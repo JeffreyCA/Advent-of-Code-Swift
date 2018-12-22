b7dc8836e6451691f9368920fd0569c28aa0ca3c
JeffreyCA <jeffreyca16@gmail.com>
2018-12-21 23:21:35 -0500

    Complete Day 18 Part 2

--- Day 18/main.swift
+++ Day 18/main.swift
@@ -5,79 +5,80 @@
 
 import Foundation
 
+// Determine number of adjacent acres that are of the given type in a piece of land
 func adjacentAcres(_ land: [[Character]], _ type: Character, _ x: Int, _ y: Int) -> Int {
     var adjacent = 0
     
-    if y - 1 >= 0 && land[y - 1][x] == type {
-        adjacent += 1
-    }
-    
-    if y - 1 >= 0 && x + 1 < land[y].count && land[y - 1][x + 1] == type {
-        adjacent += 1
-    }
-    
-    if x + 1 < land[y].count && land[y][x + 1] == type {
-        adjacent += 1
-    }
-    
-    if x + 1 < land[y].count && y + 1 < land.count && land[y + 1][x + 1] == type {
-        adjacent += 1
-    }
-    
-    if y + 1 < land.count && land[y + 1][x] == type {
-        adjacent += 1
-    }
-    
-    if y + 1 < land.count && x - 1 >= 0 && land[y + 1][x - 1] == type {
-        adjacent += 1
-    }
-    
-    if x - 1 >= 0 && land[y][x - 1] == type {
-        adjacent += 1
-    }
-    
-    if x - 1 >= 0 && y - 1 >= 0 && land[y - 1][x - 1] == type {
-        adjacent += 1
+    for curY in (y - 1) ... (y + 1) {
+        for curX in (x - 1) ... (x + 1) {
+            if (curX != x || curY != y) && curX >= 0 && curX < land[y].count && curY >= 0 && curY < land.count &&
+                land[curY][curX] == type {
+                adjacent += 1
+            }
+        }
     }
     
     return adjacent
 }
 
+// Simulate evolution of land makign use of cache
+func simulate(land: [[Character]], for time: Int) -> [[Character]] {
+    var land = land
+    var cache = [[[Character]]: Int]()
+    
+    for currentMinute in 1 ... time {
+        var original = land
+        
+        for y in 0 ..< land.count {
+            for x in 0 ..< land[y].count {
+                // Handle 3 cases as described in problem
+                if original[y][x] == "." && adjacentAcres(original, "|", x, y) >= 3 {
+                    land[y][x] = "|"
+                } else if original[y][x] == "|" && adjacentAcres(original, "#", x, y) >= 3 {
+                    land[y][x] = "#"
+                } else if original[y][x] == "#" && (adjacentAcres(original, "#", x, y) == 0 || 
+                        adjacentAcres(original, "|", x, y) == 0) {
+                    land[y][x] = "."
+                }
+            }
+        }
+        
+        if let cachedMinute = cache[land] {
+            // Previously encountered same land layout, so return the corresponding result
+            let diff = currentMinute - cachedMinute
+            let offset = (time - cachedMinute) % diff
+            let cachedLand = cache.filter({ $0.value == cachedMinute + offset}).first!.key
+            land = cachedLand
+            break
+        } else {
+            // Otherwise add current layout to cache
+            cache[land] = currentMinute
+        } 
+    }
+    
+    return land
+}
+
 func main() {
     let input = readInput()
+    // 2D Character grid representing acres of land
     var land = [[Character]]()
     
     for line in input {
         land.append(Array(line))
     }
     
-    for _ in 1 ... 10 {
-        var original = land
-        
-        for y in 0 ..< land.count {
-            for x in 0 ..< land[y].count {
-                if original[y][x] == "." && adjacentAcres(original, "|", x, y) >= 3 {
-                    land[y][x] = "|"
-                } else if original[y][x] == "|" && 
-                    adjacentAcres(original, "#", x, y) >= 3 {
-                    land[y][x] = "#"
-                } else if original[y][x] == "#" && 
-                    (adjacentAcres(original, "#", x, y) == 0 || 
-                    adjacentAcres(original, "|", x, y) == 0) {
-                    land[y][x] = "."
-                }
-                
-                
-            }
-        }
-    }
+    // Part 1
+    let afterTen = simulate(land: land, for: 10)
+    var woodedAcres = afterTen.map({ $0.filter({ $0 == "|" }).count }).reduce(0, +)
+    var lumberyards = afterTen.map({ $0.filter({ $0 == "#" }).count }).reduce(0, +)
+    print("Total resource value after 10 minutes: \(woodedAcres * lumberyards)")
     
-    let woodedAcres = land.map({ $0.filter({ $0 == "|" }).count }).reduce(0, +)
-    let lumberyards = land.map({ $0.filter({ $0 == "#" }).count }).reduce(0, +)
-    
-    print("Wooded acres: \(woodedAcres)")
-    print("Lumberyards: \(lumberyards)")
-    print("Total resource value: \(woodedAcres * lumberyards)")
+    // Part 2
+    let afterBillion = simulate(land: land, for: 1000000000)
+    woodedAcres = afterBillion.map({ $0.filter({ $0 == "|" }).count }).reduce(0, +)
+    lumberyards = afterBillion.map({ $0.filter({ $0 == "#" }).count }).reduce(0, +)
+    print("Total resource value after 1 billion minutes: \(woodedAcres * lumberyards)")
 }
 
 
