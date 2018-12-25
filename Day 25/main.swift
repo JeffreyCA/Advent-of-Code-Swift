0839f8128018814e86abd378d51346827f502b96
JeffreyCA <jeffreyca16@gmail.com>
2018-12-25 15:28:02 -0500

    Complete Day 25 Part 1

---
+++ Day 25/main.swift
@@ -0,0 +1,32 @@
+//
+//  Advent of Code 2018 - Day 25
+//  Author: JeffreyCA
+//
+
+import Foundation
+
+func main() {
+    let input = readInput()
+    
+    var points = Set<Point4>()
+    var unionFind = UnionFind<Point4>()
+    
+    for line in input {
+        let m = matches(for: "-*\\d+", in: line).map({ Int($0)! })
+        let point = Point4(m[0], m[1], m[2], m[3])
+        points.insert(point)
+        unionFind.addSetWith(point)
+    }
+    
+    for vertex in points {
+        for vertex2 in points {
+            if vertex != vertex2 && vertex.manhattanDistance(vertex2) <= 3 {
+                unionFind.unionSetsContaining(vertex, and: vertex2)
+            }
+        }
+    }
+    
+    print("Constellations: \(unionFind.count())")
+}
+
+main()
