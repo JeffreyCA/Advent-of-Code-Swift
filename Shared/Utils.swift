8647ec9af1484e8895872ff84ba8b2d37e66f806
JeffreyCA <jeffreyca16@gmail.com>
2018-12-25 15:27:44 -0500

    Add Union-Find structure

--- Shared/Utils.swift
+++ Shared/Utils.swift
@@ -7,6 +7,84 @@ import Foundation
 
 let ALPHABET_LOWER = "abcdefghijklmnopqrstuvwxyz"
 
+// Union-Find data structure
+// https://github.com/raywenderlich/swift-algorithm-club/tree/master/Union-Find/UnionFind.playground/Sources
+public struct UnionFind<T: Hashable> {
+    private var index = [T: Int]()
+    private var parent = [Int]()
+    private var size = [Int]()
+    private var setSize = 0
+    
+    public mutating func addSetWith(_ element: T) {
+        index[element] = parent.count
+        parent.append(parent.count)
+        size.append(1)
+        setSize += 1
+    }
+    
+    private mutating func setByIndex(_ index: Int) -> Int {
+        if parent[index] == index {
+            return index
+        } else {
+            parent[index] = setByIndex(parent[index])
+            return parent[index]
+        }
+    }
+    
+    public mutating func setOf(_ element: T) -> Int? {
+        if let indexOfElement = index[element] {
+            return setByIndex(indexOfElement)
+        } else {
+            return nil
+        }
+    }
+    
+    public mutating func unionSetsContaining(_ firstElement: T, and secondElement: T) {
+        if let firstSet = setOf(firstElement), let secondSet = setOf(secondElement) {
+            if firstSet != secondSet {
+                parent[firstSet] = secondSet
+                size[secondSet] += size[firstSet]
+                setSize -= 1
+            }
+        }
+    }
+    
+    public mutating func inSameSet(_ firstElement: T, and secondElement: T) -> Bool {
+        if let firstSet = setOf(firstElement), let secondSet = setOf(secondElement) {
+            return firstSet == secondSet
+        } else {
+            return false
+        }
+    }
+    
+    public func count() -> Int {
+        return setSize
+    }
+}
+
+struct Point4: Hashable {
+    var w: Int
+    var x: Int
+    var y: Int
+    var z: Int
+    
+    init(_ w: Int, _ x: Int, _ y: Int, _ z: Int) {
+        self.w = w
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+    
+    func manhattanDistance(_ p: Point4) -> Int {
+        let diffW = abs(p.w - self.w)
+        let diffX = abs(p.x - self.x)
+        let diffY = abs(p.y - self.y)
+        let diffZ = abs(p.z - self.z)
+        
+        return diffW + diffX + diffY + diffZ
+    }
+}
+
 struct Point : Hashable, CustomStringConvertible {
     var x: Int
     var y: Int
